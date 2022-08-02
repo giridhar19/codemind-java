886c181ee0d0a2796424e8080494529b7d09a5b6
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-02 22:42:01 +0530

    import java.util.*; class Teju {     public static void main(String args[])     {         int a,b;         int mod;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         mod=(int)a%b;         System.out.print(mod);
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Teju
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int mod;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        mod=(int)a%b;
+        System.out.print(mod);
+        
+    }
+}
