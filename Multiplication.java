723c23cf42cc45be32489802737f4962b539ee96
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-02 22:39:25 +0530

    import java.util.*; class Teju {     public static void main(String args[])     {         int a,b;         int mul;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         mul=(int)a*b;         System.out.print(mul);
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Teju
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int mul;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        mul=(int)a*b;
+        System.out.print(mul);
+        
+    }
+}
