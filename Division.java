b92a9bd3cf15650554efd5f8476e39914bdef2bf
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-02 22:37:39 +0530

    import java.util.*; class Teju {     public static void main(String args[])     {         int a,b;         int div;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         div=(int)a/b;         System.out.print(div);
        }
    }

---
+++ Division.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Teju
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int div;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        div=(int)a/b;
+        System.out.print(div);
+        
+    }
+}
