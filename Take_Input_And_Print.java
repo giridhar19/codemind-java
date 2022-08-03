9b8ddaec2afdd6212889134cab8c8648cc303b46
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-03 20:18:38 +0530

    import java.util.*; class sirisha {     public static void main(String args[])     {         String neww;         Scanner sc=new Scanner(System.in);         neww=sc.nextLine();         System.out.println(neww);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sirisha
+{
+    public static void main(String args[])
+    {
+        String neww;
+        Scanner sc=new Scanner(System.in); 
+        neww=sc.nextLine();
+        System.out.println(neww);
+    }
+}
