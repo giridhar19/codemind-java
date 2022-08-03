e652420e375ac6729f1c4cbe82dfc44862e7ceb3
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-03 20:24:22 +0530

    import java.util.*; class technialhub {     public static void main(String args[])     {         int a,b;         int add;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         add=(int)a+b;         System.out.print(add);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class technialhub
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int add;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        add=(int)a+b;
+        System.out.print(add);
+    }
+}
