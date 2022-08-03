6e524077e5d91ff3dc8995468e7e9e060e38fd3a
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-03 20:07:28 +0530

    import java.util.*; class Teju {     public static void main(String args[])     {         float a,b;         float mulflo;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();         mulflo=(float)(a*b);         System.out.format("%.2f",mulflo);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Teju
+{
+    public static void main(String args[])
+    {
+        float a,b;
+        float mulflo;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        mulflo=(float)(a*b);
+        System.out.format("%.2f",mulflo);
+    }
+}
