4b124ccad31568764cab32af9fee9e961a13c9bb
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-03 20:47:14 +0530

    import java.util.*; class Jagath {   public static void main(String args[])   {     int a;     float inthe,oho;     Scanner sc=new Scanner(System.in);     a=sc.nextInt();     oho=(float)(a*9)/5;     inthe=(float)(32+oho);     System.out.format("%.2f",inthe);   } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Jagath
+{
+  public static void main(String args[])
+  {
+    int a;
+    float inthe,oho;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    oho=(float)(a*9)/5;
+    inthe=(float)(32+oho);
+    System.out.format("%.2f",inthe);
+  }
+}
