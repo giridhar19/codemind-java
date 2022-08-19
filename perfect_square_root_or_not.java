604b9a8a978a369e34f475b6e9bd9d0e21b67bb8
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-19 13:58:16 +0530

    import java.util.*; class loop {   public static void main(String args[])   {     int n;     Scanner sc=new Scanner(System.in);     n=sc.nextInt();     double d=Math.sqrt(n);
        if ((d-(int)d)>0)
            System.out.println("False");
        else
            System.out.println("True");
      }
    }

---
+++ perfect_square_root_or_not.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class loop
+{
+  public static void main(String args[])
+  {
+    int n;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    double d=Math.sqrt(n);
+    
+    if ((d-(int)d)>0)
+        System.out.println("False");
+    else
+        System.out.println("True");
+  }
+}
