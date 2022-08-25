e6abe020a21b192f95cf3ad748391aa31cb26c8f
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-08-25 15:37:24 +0530

    import java.util.*; class Method {   public static void main(String args[])   {   	Scanner sc=new Scanner(System.in);   	char ch;   	ch=sc.nextLine().charAt(0);   	int asciivalue=ch;   	System.out.println(asciivalue);   } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Method
+{
+  public static void main(String args[])
+  {
+  	Scanner sc=new Scanner(System.in);
+  	char ch;
+  	ch=sc.nextLine().charAt(0);
+  	int asciivalue=ch;
+  	System.out.println(asciivalue);
+  }
+}
