188869525f67189a2af85814956dd2c7d98f2ec8
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-09-09 14:15:09 +0530

    import java.util.*; class pat { public static void main(String args[])  {   Scanner sc=new Scanner(System.in);   int r,c,k=1;   r=sc.nextInt();   c=r;   for (int i=1; i<=r; i++) //rows     {       for(int j=1;j<=c;j++)   //col       {         System.out.print((char)(64+i)+" ");       }       System.out.println();     }   } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,16 +4,15 @@ class pat
 public static void main(String args[])
  {
   Scanner sc=new Scanner(System.in);
-  int r,k;
+  int r,c,k=1;
   r=sc.nextInt();
-  k=r;
+  c=r;
   for (int i=1; i<=r; i++) //rows
     {
-      for(int j=1;j<=k;j++)   //col
+      for(int j=1;j<=c;j++)   //col
       {
-        System.out.print((char)(64+k)+" ");
+        System.out.print((char)(64+i)+" "); 
       }
-      k--;
       System.out.println();
     }
   }
