b5ef175f9801858076d16cf8a7412586fadccdc6
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-09-12 14:56:19 +0530

    import java.util.*; class Coder {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if (n%3==0)             System.out.print("Pling");         if (n%5==0)             System.out.print("Plang");         if (n%7==0)             System.out.print("Plong");         if (n%3!=0&&n%5!=0&&n%7!=0)             System.out.print(n);     } }

---
+++ RainDrop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Coder
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if (n%3==0)
+            System.out.print("Pling");
+        if (n%5==0)
+            System.out.print("Plang");
+        if (n%7==0)
+            System.out.print("Plong");
+        if (n%3!=0&&n%5!=0&&n%7!=0)
+            System.out.print(n);
+    }
+}
