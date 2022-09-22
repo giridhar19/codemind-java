d3fb2f5fa5f16e31d83fd0a8d3401450b3f642ab
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-09-22 14:37:57 +0530

    import java.util.*; class oddindex {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,x[],co=0,od=0;         n=sc.nextInt();         x=new int[n];         for (i=0;i<n;i++)             x[i]=sc.nextInt();         for (i=0;i<n;i++)             {                 if (i%2!=0)                     co=co+x[i];                 else                     od=od+x[i];             }
            System.out.println(od-co);
        }
    }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class oddindex
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,x[],co=0,od=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for (i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        for (i=0;i<n;i++)
+            {
+                if (i%2!=0)
+                    co=co+x[i];
+                else
+                    od=od+x[i];
+            }
+        
+        System.out.println(od-co);
+    }
+}
