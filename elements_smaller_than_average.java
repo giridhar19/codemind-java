58c1a877f31bce3ac9b99fd1402eb4c1ff9530b4
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-09-23 14:27:05 +0530

    import java.util.*; class oddindex {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,x[],l=0,c=0;         n=sc.nextInt();         x=new int[n];         for (i=0;i<n;i++)             x[i]=sc.nextInt();
            for (i=0;i<n;i++)
                {
                 l=l+x[i];   
                }
            l=l/n;
            for (i=0;i<n;i++)
                {
                    if (x[i]<=l)
                        c++;
                }
            System.out.print(c);
        }
    }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class oddindex
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,x[],l=0,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for (i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        
+        for (i=0;i<n;i++)
+            {
+             l=l+x[i];   
+            }
+        l=l/n;
+        for (i=0;i<n;i++)
+            {
+                if (x[i]<=l)
+                    c++;
+            }
+        System.out.print(c);
+    }
+}
