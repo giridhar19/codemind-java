1e7ed77b4ee49fdd01edc1c5e7b75ffc33fae64c
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-09-23 22:28:09 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int m=s.nextInt();         int o=s.nextInt();         int max=((n*m*o)/100);         System.out.println(max);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int m=s.nextInt();
+        int o=s.nextInt();
+        int max=((n*m*o)/100);
+        System.out.println(max);
+    }
+}
