feebe9ce21b853065e468d6c82178651a13b5ae2
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-10-13 14:26:56 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)             x[i]=s.nextInt();         int co=0;         for(int i=0;i<(n-1);i++)         {             if (x[i]<x[i+1])                 co++;         }         if(x[n-2]<x[n-1])             co++;         if (co==n)             System.out.print("yes");         else             System.out.print("no");     } }

--- Ascending_array.java
+++ Ascending_array.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
-class count
+import java.util.*;
+class sol
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
-        int x[],n,i;
-        n=sc.nextInt();
-        x=new int[n];
-        for (i=0;i<n;i++)
-            x[i]=sc.nextInt();
-        for (i=0;i<n-1;i++)
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+            x[i]=s.nextInt();
+        int co=0;
+        for(int i=0;i<(n-1);i++)
         {
-            if (x[i]>=x[i+1])
-            {
-                System.out.println("no");
-                System.exit(0);
-            }
+            if (x[i]<x[i+1])
+                co++;
         }
-         System.out.println("yes");
+        if(x[n-2]<x[n-1])
+            co++;
+        if (co==n)
+            System.out.print("yes");
+        else
+            System.out.print("no");
     }
 }
