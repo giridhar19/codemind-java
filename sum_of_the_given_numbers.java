85ac91e47b98ef218f43c6a5458c8909eb67b950
giridhar19 <102218921+giridhar19@users.noreply.github.com>
2022-10-28 21:37:11 +0530

    import java.util.*; class code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t,n,m,sum;         t=sc.nextInt();         while (t>0)         {             n=sc.nextInt();             m=sc.nextInt();             sum=n+m;             System.out.println(sum);             t--;         }     } }

--- sum_of_the_given_numbers.java
+++ sum_of_the_given_numbers.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-class coder
+import java.util.*;
+class code
 {
     public static void main(String args[])
     {
-        int t,a,b,sum=0;
         Scanner sc=new Scanner(System.in);
+        int t,n,m,sum;
         t=sc.nextInt();
-        while(t>0)
+        while (t>0)
         {
-            a=sc.nextInt();
-            b=sc.nextInt();
-            sum=a+b;
+            n=sc.nextInt();
+            m=sc.nextInt();
+            sum=n+m;
             System.out.println(sum);
             t--;
         }
